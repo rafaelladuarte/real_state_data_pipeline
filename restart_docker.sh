824ca8f824336f78944d48247e62951fd59d05af
Rafaella Duarte <rafaella.d.d.carvalho@gmail.com>
2023-12-14 22:26:41 -0300

    add login services docker

--- restart_docker.sh
+++
@@ -1,9 +0,0 @@
-sudo docker rm -f $(docker ps -aq)
-sudo docker rmi -f $(docker images -aq)
-sudo docker volume rm $(docker volume ls -q)
-sudo docker network rm $(docker network ls -q)
-
-
-# docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' postgres
-# psql -h 172.23.0.4 -U username -d postgres -p 5433
-# sudo docker logs airflow-webserver
