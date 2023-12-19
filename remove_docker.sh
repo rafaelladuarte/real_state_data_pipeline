438bc7dfacf8a0c33a5109f2ff637cceb4d13d64
Rafaella Duarte <rafaella.d.d.carvalho@gmail.com>
2023-12-19 11:13:32 -0300

    update dockerfile and diagrams

--- remove_docker.sh
+++ remove_docker.sh
@@ -1,10 +1,16 @@
+#!/bin/bash
+
+# Parar e remover os contêineres definidos no docker-compose
 sudo docker-compose down
+
+# Remover todos os contêineres em execução
 sudo docker rm -f $(docker ps -aq)
+
+# Remover todas as imagens do Docker
 sudo docker rmi -f $(docker images -aq)
+
+# Remover todos os volumes do Docker
 sudo docker volume rm $(docker volume ls -q)
+
+# Remover todas as redes do Docker
 sudo docker network rm $(docker network ls -q)
-
-
-# docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' postgres
-# psql -h 172.23.0.4 -U username -d postgres -p 5433
-# sudo docker logs airflow-webserver
--- start_project.sh
+++ start_project.sh
@@ -1,9 +1,19 @@
+#!/bin/bash
+
+# Dar permissão de execução ao script
+chmod +x start_project.sh
+
+# Adicionar o diretório ao PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:~./brazilian_ecommerce_data_modeling/project
 
+# Iniciar os contêineres Docker em segundo plano
 sudo docker-compose up -d
 
-docker exec -it airflow-webserver /bin/bash
+# Inicializar o banco de dados do Apache Airflow
+docker exec -it airflow-webserver airflow db init
 
-airflow users create --username username --password password --firstname Admin --lastname Admin --role Admin --email admin@email.com
+# Criar um usuário no Apache Airflow
+docker exec -it airflow-webserver airflow users create --username username --password password --firstname Admin --lastname Admin --role Admin --email admin@email.com
 
+# Sair do contêiner
 exit
