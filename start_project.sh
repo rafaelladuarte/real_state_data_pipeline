8e89fedd0c33c56dcbe3ac55ba162447b481c0ad
Rafaella Duarte <rafaella.d.d.carvalho@gmail.com>
2023-12-20 19:02:40 -0300

    add dags to apache airflow

--- start_project.sh
+++ start_project.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-
-# Dar permissão de execução ao script
 chmod +x start_project.sh
-
-# Adicionar o diretório ao PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:~./brazilian_ecommerce_data_modeling/project
-
-# Iniciar os contêineres Docker em segundo plano
 sudo docker-compose up -d
-
-# Inicializar o banco de dados do Apache Airflow
-docker exec -it airflow-webserver airflow db init
-
-# Criar um usuário no Apache Airflow
-docker exec -it airflow-webserver airflow users create --username username --password password --firstname Admin --lastname Admin --role Admin --email admin@email.com
-
-# Sair do contêiner
-exit
